d934f346b352b3bc60e6e56bbaecc799ae77426e
Sakshi Maheshwari <97123983+SakshiMaheshwari24@users.noreply.github.com>
2023-04-15 00:57:14 +0530

    Add files via upload

---
+++ flag.py
@@ -0,0 +1,79 @@
+import turtle
+
+# Set up the turtle window and set the background color
+turtle.setup(width=800, height=500)
+turtle.bgcolor("#FFFFFF")
+
+# Define the height and width of the flag
+height = 300
+width = 450
+
+# Draw the saffron rectangle
+turtle.penup()
+turtle.goto(-225, -50)
+turtle.pendown()
+turtle.color("#FF9933")
+turtle.begin_fill()
+turtle.forward(width)
+turtle.right(90)
+turtle.forward(height/3)
+turtle.right(90)
+turtle.forward(width)
+turtle.right(90)
+turtle.forward(height/3)
+turtle.end_fill()
+
+# Draw the white rectangle
+turtle.penup()
+turtle.goto(-225, 50)
+turtle.pendown()
+turtle.color("#FFFFFF")
+turtle.begin_fill()
+turtle.forward(width)
+turtle.right(90)
+turtle.forward(height/3)
+turtle.right(90)
+turtle.forward(width)
+turtle.right(90)
+turtle.forward(height/3)
+turtle.end_fill()
+
+
+# Draw the green rectangle
+turtle.penup()
+turtle.goto(225, 105)
+turtle.pendown()
+turtle.color("#138808")
+turtle.begin_fill()
+turtle.forward(width)
+turtle.right(90)
+turtle.forward(height/3)
+turtle.right(90)
+turtle.forward(width)
+turtle.right(90)
+turtle.forward(height/3)
+turtle.end_fill()
+
+# Draw the circle in the center
+turtle.penup()
+turtle.goto(-65, 25)
+turtle.pendown()
+turtle.color("#000080")
+turtle.begin_fill()
+turtle.circle(75)
+turtle.end_fill()
+
+# Draw the 24 spokes of the wheel
+turtle.penup()
+turtle.goto(10, 25)
+turtle.pendown()
+turtle.color("#FFFFFF")
+turtle.pensize(2)
+for i in range(24):
+    turtle.forward(75)
+    turtle.backward(75)
+    turtle.left(15)
+
+# Hide the turtle and update the screen
+turtle.hideturtle()
+turtle.done()
