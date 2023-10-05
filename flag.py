c3673322c96e67636f2d18a20076ebf8ca684928
Sakshi Maheshwari <97123983+SakshiMaheshwari24@users.noreply.github.com>
2023-10-05 16:04:32 +0530

    Update flag.py

--- flag.py
+++ flag.py
@@ -2,17 +2,18 @@ import turtle
 
 # Set up the turtle window and set the background color
 turtle.setup(width=800, height=500)
-turtle.bgcolor("#FFFFFF")
+turtle.bgcolor("#E0FFFF")
 
 # Define the height and width of the flag
 height = 300
 width = 450
 
-# Draw the saffron rectangle
+# Draw the green rectangle
 turtle.penup()
 turtle.goto(-225, -50)
 turtle.pendown()
-turtle.color("#FF9933")
+turtle.color("#138808")
+#turtle.color("#FF9933")
 turtle.begin_fill()
 turtle.forward(width)
 turtle.right(90)
@@ -25,25 +26,26 @@ turtle.end_fill()
 
 # Draw the white rectangle
 turtle.penup()
-turtle.goto(-225, 50)
+turtle.goto(-225, -47)
 turtle.pendown()
 turtle.color("#FFFFFF")
 turtle.begin_fill()
-turtle.forward(width)
+turtle.forward(width/3)
 turtle.right(90)
-turtle.forward(height/3)
+turtle.forward(height*1.5)
 turtle.right(90)
-turtle.forward(width)
+turtle.forward(width/3)
 turtle.right(90)
-turtle.forward(height/3)
+turtle.forward(height*1.5)
 turtle.end_fill()
 
 
-# Draw the green rectangle
+# Draw the saffron rectangle
 turtle.penup()
 turtle.goto(225, 105)
 turtle.pendown()
-turtle.color("#138808")
+turtle.color("#FF9933")
+#turtle.color("#138808")
 turtle.begin_fill()
 turtle.forward(width)
 turtle.right(90)
@@ -56,7 +58,7 @@ turtle.end_fill()
 
 # Draw the circle in the center
 turtle.penup()
-turtle.goto(-65, 25)
+turtle.goto(-65, 27)
 turtle.pendown()
 turtle.color("#000080")
 turtle.begin_fill()
